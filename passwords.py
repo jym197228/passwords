085c0653bfffa45e0bc87a820f0cc56c38d3fe57
Chris Chung <jym197@gmail.com>
2020-08-21 15:30:34 +0800

    用更好的邏輯編寫

--- passwords.py
+++ passwords.py
@@ -1,15 +1,16 @@
 # 密碼重試
 # 正確密碼為a123456
-x = 0
+x = 3 # 剩餘次數
+pwd = 'a123456' #正確密碼
 while x < 3:
 	password = input('請輸入密碼: ')
-	if password == 'a123456':
+	if password == pwd:
 		print('登入成功!')
 		break
 	else: 
-		x = x + 1
-		if x == 3:
+		x = x - 1
+		if x == 0:
 			print('密碼已被鎖定，請洽櫃台解鎖!')
 			break
 		else:
-			print('密碼錯誤! 還有', 3 - x, '次機會。')
+			print('密碼錯誤! 還有', x, '次機會。')
